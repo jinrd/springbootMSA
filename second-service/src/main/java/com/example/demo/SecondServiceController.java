f62ca6ffe6bf8ae788a88765e5996870b4d8c861
jinrd <gkwlsdnjs95@gmail.com>
2023-10-11 22:59:57 +0900

    spring cloud gateway 를 사용했을 때 주의점 포함 chapter 2-6

--- second-service/src/main/java/com/example/demo/SecondServiceController.java
+++ second-service/src/main/java/com/example/demo/SecondServiceController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/second-service/") // apigateway-service 에서 yml Path 정의를 수정했기 때문에 맞춰준다.
 public class SecondServiceController {
 	
 	@GetMapping("/welcome")
