f60cf4a4fe5490fd816dd9f6bd0eb242e360564d
jinrd <gkwlsdnjs95@gmail.com>
2023-10-12 10:57:39 +0900

    java 와 yml 을 통해 gateway route 시도해봄

--- first-service/src/main/java/com/example/demo/FirstServiceController.java
+++ first-service/src/main/java/com/example/demo/FirstServiceController.java
@@ -1,9 +1,13 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestController
 @RequestMapping("/first-service") // apigateway-service 에서 yml Path 정의를 수정했기 때문에 맞춰준다.
 public class FirstServiceController {
@@ -11,5 +15,11 @@ public class FirstServiceController {
 	@GetMapping("/welcome")
 	public String welcome() {
 		return "Welcome to the First Service";
+	}
+	
+	@GetMapping("/message")
+	public String message(@RequestHeader("first-request") String header) {
+		log.info(header);
+		return "Hello World in First Service.Message";
 	}
 }
--- second-service/src/main/java/com/example/demo/SecondServiceController.java
+++ second-service/src/main/java/com/example/demo/SecondServiceController.java
@@ -1,9 +1,13 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestController
 @RequestMapping("/second-service/") // apigateway-service 에서 yml Path 정의를 수정했기 때문에 맞춰준다.
 public class SecondServiceController {
@@ -11,6 +15,12 @@ public class SecondServiceController {
 	@GetMapping("/welcome")
 	public String welcome() {
 		return "Welcome to the Second Service";
+	}
+	
+	@GetMapping("/message")
+	public String message(@RequestHeader("second-request") String header) {
+		log.info(header);
+		return "Hello World in Second Service.Message";
 	}
 }
  
