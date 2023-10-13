385afdf7b03458bd0f3f63cb2299d57a7f4dc157
jinrd <gkwlsdnjs95@gmail.com>
2023-10-13 15:14:02 +0900

    랜덤포트로 지정후 API Gateway 에서 포워딩 하는 방법

--- first-service/src/main/java/com/example/demo/FirstServiceController.java
+++ first-service/src/main/java/com/example/demo/FirstServiceController.java
@@ -1,5 +1,10 @@
 package com.example.demo;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +16,21 @@ import lombok.extern.slf4j.Slf4j;
 @RestController
 @RequestMapping("/first-service") // apigateway-service 에서 yml Path 정의를 수정했기 때문에 맞춰준다.
 public class FirstServiceController {
+	
+	/*
+	 yml파일에 등록되어진 환설정 정보를 가져오는 방법
+	 1. Environment
+	 */
+	
+//	@Autowired
+//	Environment env;
+	
+	Environment env;
+	
+	@Autowired
+	public FirstServiceController(Environment env) {
+		this.env = env;
+	}
 	
 	@GetMapping("/welcome")
 	public String welcome() {
@@ -24,7 +44,9 @@ public class FirstServiceController {
 	}
 	
 	@GetMapping("/check")
-	public String check() {
-		return "Hi, there. This is a message from First Service.check";
+	public String check(HttpServletRequest request) {
+		log.info("ServerPort = {}", request.getServerPort());
+		
+		return String.format("Hi, there. This is a message from First Service.check on PORT %s", env.getProperty("local.server.port"));
 	}
 }
